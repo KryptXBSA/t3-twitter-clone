95203a1b40249394161aaecbac15a57d64e2cb07
Aland Sleman <alandsazkm@gmail.com>
2023-02-22 17:11:22 +0300

    Finished SidebarRight

--- src/components/SidebarRight/SidebarRight.tsx
+++ src/components/SidebarRight/SidebarRight.tsx
@@ -1,180 +1,123 @@
-import React from 'react'
+import Avatar from "@components/Avatar";
+import React from "react";
 
 export default function SidebarRight() {
   return (
-          <div className="hidden h-screen w-[290px] lg:block lg:w-[350px]">
-            <div className="fixed flex h-screen w-[290px] flex-col overflow-y-auto lg:w-[350px]">
-              {/* Search */}
-              <div className="relative m-2">
-                <div className="absolute flex h-full cursor-pointer items-center pl-4 text-gray-600">
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    className="icon icon-tabler icon-tabler-mail"
-                    width={18}
-                    height={18}
-                    viewBox="0 0 24 24"
-                    strokeWidth="1.5"
-                    stroke="currentColor"
-                    fill="none"
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                  >
-                    <path d="M21.53 20.47l-3.66-3.66C19.195 15.24 20 13.214 20 11c0-4.97-4.03-9-9-9s-9 4.03-9 9 4.03 9 9 9c2.215 0 4.24-.804 5.808-2.13l3.66 3.66c.147.146.34.22.53.22s.385-.073.53-.22c.295-.293.295-.767.002-1.06zM3.5 11c0-4.135 3.365-7.5 7.5-7.5s7.5 3.365 7.5 7.5-3.365 7.5-7.5 7.5-7.5-3.365-7.5-7.5z" />
-                  </svg>
-                </div>
-                <input
-                  className="flex h-9 w-full items-center rounded-full border border-gray-200 pl-12 text-sm font-normal text-gray-100 shadow focus:border focus:border-blue-200 focus:bg-gray-100 focus:outline-none dark:border-dim-400 "
-                  placeholder="Search Twitter"
-                />
-              </div>
-              {/* /Search */}
-              {/* What’s happening */}
-              <div className="m-2 rounded-2xl bg-main-background ">
-                <h1 className="text-md border-b border-gray-200 p-3 font-bold text-gray-900 dark:border-dim-200 dark:text-white">
-                  What’s happening
-                </h1>
-                {/* Trending Topic */}
-                <div className="duration-350 cursor-pointer border-b border-gray-200 p-3 text-sm font-normal text-blue-400 transition ease-in-out hover:bg-gray-100 dark:border-dim-200 ">
-                  <h2 className="text-md font-bold text-gray-800 dark:text-white">
-                    #FreePS5Monday
-                  </h2>
-                  <p className="text-xs text-gray-400">29.7K Tweets</p>
-                </div>
-                {/* /Trending Topic */}
-                {/* Trending Topic */}
-                <div className="duration-350 cursor-pointer border-b border-gray-200 p-3 text-sm font-normal text-blue-400 transition ease-in-out hover:bg-gray-100 dark:border-dim-200 ">
-                  <h2 className="text-md font-bold text-gray-800 dark:text-white">
-                    #BTSonGMA
-                  </h2>
-                  <p className="text-xs text-gray-400">351K Tweets</p>
-                </div>
-                {/* /Trending Topic */}
-                {/* Trending Topic */}
-                <div className="duration-350 cursor-pointer border-b border-gray-200 p-3 text-sm font-normal text-blue-400 transition ease-in-out hover:bg-gray-100 dark:border-dim-200 dark:hover:bg-dim-300">
-                  <h2 className="text-md font-bold text-gray-800 dark:text-white">
-                    #AstraZeneca
-                  </h2>
-                  <p className="text-xs text-gray-400">52.7K Tweets</p>
-                </div>
-                {/* /Trending Topic */}
-                <div className="duration-350 cursor-pointer p-3 text-sm font-normal text-blue-400 transition ease-in-out hover:bg-gray-100 dark:hover:bg-dim-300">
-                  Show more
-                </div>
-              </div>
-              {/* /What’s happening */}
-              {/* Who to follow */}
-              <div className="m-2 rounded-2xl ">
-                <h1 className="text-md border-b border-gray-200 p-3 font-bold text-gray-900 dark:border-dim-200 dark:text-white">
-                  Who to follow
-                </h1>
-                {/* Twitter Account */}
-                <div className="duration-350 cursor-pointer border-b border-gray-200 p-3 text-sm font-normal text-blue-400 transition ease-in-out hover:bg-gray-100 dark:border-dim-200 dark:hover:bg-dim-300">
-                  <div className="flex flex-row justify-between p-2">
-                    <div className="flex flex-row">
-                      <img
-                        className="h-10 w-10 rounded-full"
-                        src="https://pbs.twimg.com/profile_images/1308769664240160770/AfgzWVE7_normal.jpg"
-                        alt="Joe Biden"
-                      />
-                      <div className="ml-2 flex flex-col">
-                        <h1 className="text-sm font-bold text-gray-900 dark:text-white">
-                          Joe Biden
-                        </h1>
-                        <p className="text-sm text-gray-400">@JoeBiden</p>
-                      </div>
-                    </div>
-                    <div className="">
-                      <div className="flex h-full items-center text-gray-800 dark:text-white">
-                        <a
-                          href="#"
-                          className="rounded-full border-2 border-blue-400 px-4 py-1 text-xs font-bold text-blue-400"
-                        >
-                          Follow
-                        </a>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-                {/* /Twitter Account */}
-                {/* Twitter Account */}
-                <div className="duration-350 cursor-pointer border-b border-gray-200 p-3 text-sm font-normal text-blue-400 transition ease-in-out hover:bg-gray-100 dark:border-dim-200 dark:hover:bg-dim-300">
-                  <div className="flex flex-row justify-between p-2">
-                    <div className="flex flex-row">
-                      <img
-                        className="h-10 w-10 rounded-full"
-                        src="https://pbs.twimg.com/profile_images/1308769664240160770/AfgzWVE7_normal.jpg"
-                        alt="Joe Biden"
-                      />
-                      <div className="ml-2 flex flex-col">
-                        <h1 className="text-sm font-bold text-gray-900 dark:text-white">
-                          Joe Biden
-                        </h1>
-                        <p className="text-sm text-gray-400">@JoeBiden</p>
-                      </div>
-                    </div>
-                    <div className="">
-                      <div className="flex h-full items-center text-gray-800 dark:text-white">
-                        <a
-                          href="#"
-                          className="rounded-full border-2 border-blue-400 px-4 py-1 text-xs font-bold text-blue-400"
-                        >
-                          Follow
-                        </a>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-                {/* /Twitter Account */}
-                {/* Loader */}
-                <div className="mx-auto w-full max-w-sm border-b border-gray-200 p-4 dark:border-dim-200">
-                  <div className="flex animate-pulse space-x-4">
-                    <div className="h-12 w-12 rounded-full bg-gray-400" />
-                    <div className="flex-1 space-y-4 py-1">
-                      <div className="h-4 w-3/4 rounded bg-gray-400" />
-                      <div className="space-y-2">
-                        <div className="h-4 rounded bg-gray-400" />
-                        <div className="h-4 w-5/6 rounded bg-gray-400" />
-                      </div>
-                    </div>
-                  </div>
-                </div>
-                {/* /Loader */}
-                <div className="duration-350 cursor-pointer p-3 text-sm font-normal text-blue-400 transition ease-in-out hover:bg-gray-100 dark:hover:bg-dim-300">
-                  Show more
-                </div>
-              </div>
-              {/* /Who to follow */}
-              <footer>
-                <ul className="my-4 mx-2 text-xs text-gray-500">
-                  <li className="mx-2 inline-block">
-                    <a className="hover:underline" href="#">
-                      Terms of Service
-                    </a>
-                  </li>
-                  <li className="mx-2 inline-block">
-                    <a className="hover:underline" href="#">
-                      Privacy Policy
-                    </a>
-                  </li>
-                  <li className="mx-2 inline-block">
-                    <a className="hover:underline" href="#">
-                      Cookie Policy
-                    </a>
-                  </li>
-                  <li className="mx-2 inline-block">
-                    <a className="hover:underline" href="#">
-                      Ads info
-                    </a>
-                  </li>
-                  <li className="mx-2 inline-block">
-                    <a className="hover:underline" href="#">
-                      More
-                    </a>
-                  </li>
-                  <li className="mx-2 inline-block">© 2020 Twitter, Inc.</li>
-                </ul>
-              </footer>
-            </div>
-          </div>
-  )
+    <div className="hidden h-screen w-[290px] lg:block lg:w-[350px]">
+      <div className="fixed flex h-screen w-[290px] flex-col overflow-y-auto lg:w-[350px]">
+        <SearchBar />
+        <TrendsForYou />
+        <WhoToFollow />
+        <Loader />
+      </div>
+    </div>
+  );
 }
+function SearchBar() {
+  return (
+    <div className="relative m-2">
+      <div className="absolute   flex h-full cursor-pointer items-center pl-4 text-gray-600">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          className="icon icon-tabler icon-tabler-mail"
+          width={18}
+          height={18}
+          viewBox="0 0 24 24"
+          strokeWidth="1.5"
+          stroke="currentColor"
+          fill="none"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+        >
+          <path d="M21.53 20.47l-3.66-3.66C19.195 15.24 20 13.214 20 11c0-4.97-4.03-9-9-9s-9 4.03-9 9 4.03 9 9 9c2.215 0 4.24-.804 5.808-2.13l3.66 3.66c.147.146.34.22.53.22s.385-.073.53-.22c.295-.293.295-.767.002-1.06zM3.5 11c0-4.135 3.365-7.5 7.5-7.5s7.5 3.365 7.5 7.5-3.365 7.5-7.5 7.5-7.5-3.365-7.5-7.5z" />
+        </svg>
+      </div>
+      <input
+        className=" flex h-9 w-full items-center rounded-full bg-[#202327] p-6 pl-12 text-sm font-normal text-gray-100 "
+        placeholder="Search Twitter"
+      />
+    </div>
+  );
+}
+
+const TrendingTopic = ({ hashtag, tweets }: {hashtag:string,tweets:string}) => {
+  return (
+    <div
+      className={`main-border sidebar-hover dark:border-dim-200 cursor-pointer p-3 text-sm font-normal text-blue-400 transition ease-in-out`}
+    >
+      <h2 className="text-md font-bold text-gray-800 dark:text-white">
+        {hashtag}
+      </h2>
+      <p className="text-xs text-gray-400">{tweets} Tweets</p>
+    </div>
+  );
+};
+
+const TwitterAccount = ({ name, username, imgUrl }: {name:string,username:string,imgUrl:string}) => {
+  return (
+    <div
+      className={`main-border sidebar-hover dark:border-dim-200 cursor-pointer p-3 text-sm font-normal text-blue-400 transition ease-in-out`}
+    >
+      <div className="flex flex-row justify-between p-2">
+        <div className="flex flex-row">
+          <Avatar size={48} />
+          <div className="ml-2 flex flex-col">
+            <h1 className="text-sm font-bold text-gray-900 dark:text-white">
+              {name}
+            </h1>
+            <p className="text-sm text-gray-400">@{username}</p>
+          </div>
+        </div>
+        <div className="">
+          <div className="flex h-full items-center text-gray-800 dark:text-white">
+            <a
+              href="#"
+              className="rounded-full border-2 border-blue-400 px-4 py-1 text-xs font-bold text-blue-400"
+            >
+              Follow
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+const Loader = () => {
+  return (
+    <div
+      className={`main-border dark:border-dim-200 mx-auto w-full max-w-sm border-b border-gray-200 p-4`}
+    >
+      <div className="flex animate-pulse space-x-4">
+        <div className="h-4 w-3/4 rounded bg-gray-400"></div>
+        <div className="h-4 w-1/4 rounded bg-gray-400"></div>
+      </div>
+    </div>
+  );
+};
+
+const TrendsForYou = () => {
+  return (
+    <div className={`main-border sidebar-bg m-2 rounded-2xl`}>
+      <h1 className="p-3 text-lg font-bold text-gray-900 dark:text-white">
+        Trends for you
+      </h1>
+      <TrendingTopic hashtag="#FreePS5Monday" tweets="29.7K" />
+      <TrendingTopic hashtag="#BTSonGMA" tweets="351K" />
+      <TrendingTopic hashtag="#AstraZeneca" tweets="52.7K" />
+    </div>
+  );
+};
+
+const WhoToFollow = () => {
+  return (
+    <div className={`sidebar-bg m-2 rounded-2xl`}>
+      <h1 className="p-3  text-lg font-bold text-gray-900 dark:text-white">
+        Who to follow
+      </h1>
+      <TwitterAccount name="Aland Sleman" username="AlandSleman" />
+      <TwitterAccount name="Jane" />
+      <TwitterAccount name="John" />
+    </div>
+  );
+};
